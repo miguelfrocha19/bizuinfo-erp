e6f921ed80b7d69640ac578e2232bc44a2b8809a
Miguel <miguelf.rocha19@gmail.com>
2026-06-01 20:34:19 -0300

    Initial commit

---
+++ src/main/java/com/bizuinfo/acesso/bean/LogAuditoriaBean.java
@@ -0,0 +1,30 @@
+package com.bizuinfo.acesso.bean;
+
+import com.bizuinfo.usuario.dao.LogAuditoriaDAO;
+import com.bizuinfo.usuario.model.LogAuditoria;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class LogAuditoriaBean implements Serializable {
+
+    @Inject
+    private LogAuditoriaDAO logAuditoriaDAO;
+
+    private List<LogAuditoria> logs;
+
+    @PostConstruct
+    public void init() {
+        logs = logAuditoriaDAO.listarTodos();
+    }
+
+    public List<LogAuditoria> getLogs() {
+        return logs;
+    }
+}
